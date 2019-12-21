fd56a733fc87d757bcbc0f40f46047df5bd6b2a0
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-12-21 00:32:27 -0800

    add BookService

---
+++ src/main/java/com/thanhhnguyen23/spring/service/BookServiceImpl.java
@@ -0,0 +1,47 @@
+package com.thanhhnguyen23.spring.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thanhhnguyen23.spring.dao.BookDAO;
+import com.thanhhnguyen23.spring.model.Book;
+
+@Service
+public class BookServiceImpl implements BookService{
+
+	@Autowired
+	private BookDAO bookDAO;
+
+	@Override
+	public long save(Book book) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Book get(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	@Transactional // TODO - explore internals
+	public List<Book> list() {
+		return bookDAO.list();
+	}
+
+	@Override
+	public void update(long id, Book book) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
