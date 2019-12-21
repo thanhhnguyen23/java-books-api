28aaed2ef74eb655e844260b5ec4210eff10bec2
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-12-20 23:15:12 -0800

    add configs

---
+++ src/main/java/com/thanhhnguyen23/spring/config/AppConfig.java
@@ -0,0 +1,72 @@
+package com.thanhhnguyen23.spring.config;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import static org.hibernate.cfg.AvailableSettings.*;
+
+/**
+ * reads and set property configs from resources folder
+ * (src/main/resources/db.properties)
+ */
+@Configuration
+@PropertySource("classpath:db.properties")
+@EnableTransactionManagement // TODO -- explore internals
+@ComponentScans(value = { // TODO -- explore internals
+		@ComponentScan("com.thanhhnguyen23.spring.dao"), // packages of beans/dependencies
+		@ComponentScan("com.thanhhnguyen23.spring.service"),// packages of beans/dependencies
+
+})
+public class AppConfig {
+
+	@Autowired // Spring container will create env object with this annotation
+	private Environment env;
+
+	@Bean
+	public LocalSessionFactoryBean getSessionFactory() {
+
+		LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+		Properties props = new Properties();
+		props.put(DRIVER, env.getProperty("mysql.driver"));
+		props.put(URL, env.getProperty("mysql.url"));
+		props.put(USER, env.getProperty("mysql.user"));
+		props.put(PASS, env.getProperty("mysql.pw"));
+
+		// hibernate properties
+		props.put(SHOW_SQL, env.getProperty("hibernate.show_sql"));
+		props.put(HBM2DDL_AUTO, env.getProperty("hibernate.hbm2ddl.auto"));
+
+		// c3p0 properties
+		props.put(C3P0_MIN_SIZE, env.getProperty("hibernate.c3p0.min_size"));
+		props.put(C3P0_MAX_SIZE, env.getProperty("hibernate.c3p0.max_size"));
+
+		props.put(C3P0_ACQUIRE_INCREMENT, env.getProperty("hibernate.c3p0.acquire_increment"));
+		props.put(C3P0_TIMEOUT, env.getProperty("hibernate.c3p0.timeout"));
+		props.put(C3P0_MAX_STATEMENTS, env.getProperty("hibernate.c3p0.max_statements"));
+
+		// factoryBean properties
+		factoryBean.setHibernateProperties(props);
+		factoryBean.setPackagesToScan("com.thanhhnguyen23.spring.model");
+
+		return factoryBean;
+	}
+
+	@Bean
+	public HibernateTransactionManager getTransactionManager() {
+
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+		transactionManager.setSessionFactory(getSessionFactory().getObject());
+
+		return transactionManager;
+	}
+}
---
+++ src/main/java/com/thanhhnguyen23/spring/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.thanhhnguyen23.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.thanhhnguyen23.spring.controller"})
+public class WebConfig extends WebMvcConfigurerAdapter{ 
+
+}
