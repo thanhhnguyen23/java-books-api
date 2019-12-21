2568bb6c1ec4bc3105b22cfbbc13589fab167d70
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-12-21 00:08:13 -0800

    revise app config and pom.xml

--- src/main/java/com/thanhhnguyen23/spring/config/AppConfig.java
+++ src/main/java/com/thanhhnguyen23/spring/config/AppConfig.java
@@ -45,6 +45,7 @@ public class AppConfig {
 		// hibernate properties
 		props.put(SHOW_SQL, env.getProperty("hibernate.show_sql"));
 		props.put(HBM2DDL_AUTO, env.getProperty("hibernate.hbm2ddl.auto"));
+		props.put(DIALECT, env.getProperty("hibernate.dialect"));
 
 		// c3p0 properties
 		props.put(C3P0_MIN_SIZE, env.getProperty("hibernate.c3p0.min_size"));
